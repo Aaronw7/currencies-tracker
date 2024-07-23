c3c2936e8611e9f2127377a24bce14004fe2a2ed
AaronW <aaronwang96@hotmail.com>
2024-07-23 14:36:10 -0700

    pointers onHover

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { GeoJSONFeature } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || '';
@@ -30,14 +30,26 @@ const Map = () => {
         if (!map.current) return;
         const layerId = currency;
 
-        map.current.on('mouseenter', layerId, (e) => {
+        map.current.on('mouseenter', layerId, () => {
           if (!map.current) return;
+          console.log()
 
           map.current.setPaintProperty(layerId, 'fill-color', '#438731');
+          map.current.getCanvas().style.cursor = 'pointer';
         });
 
-        map.current.on('mouseleave', layerId, () => {
+        map.current.on('mouseleave', layerId, (e) => {
           if (!map.current) return;
+
+          const features = map.current.queryRenderedFeatures(e.point, {
+            layers: currencyAbbreviations
+          }) as GeoJSONFeature[];
+
+          if (features.length > 0 && features[0].layer) {
+            map.current.getCanvas().style.cursor = 'pointer';
+          } else {
+            map.current.getCanvas().style.cursor = 'grab';
+          }
 
           map.current.setPaintProperty(layerId, 'fill-color', '#86cf72');
         });
