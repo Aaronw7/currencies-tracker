c2c39547940c3c2c334f7c62796521a223bf711c
AaronW <aaronwang96@hotmail.com>
2024-07-29 17:24:56 -0700

    font preload

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,6 +5,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.png" sizes="any" color="white" />
+        <link
+          rel="preload"
+          href="/path/to/font.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
         <meta name="author" content="Aaron W" />
       </Head>
       <body>
